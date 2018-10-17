5c5e23202e693a855548fc32b7d1a2b89db0a9a5
Javier <xaviersaltos44@gmail.com>
2018-10-17 00:05:43 -0500

    Cristian39

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -87,7 +87,7 @@
 
                                 <li class="nav-item dropdown">
                                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                Varios
+                                Transacciones
                                 </a>
 
                                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
