d3f00666b0ff3e985ffbc8d5e94e3063e4497ed5
Javier <xaviersaltos44@gmail.com>
2018-10-17 00:04:16 -0500

    Cristian38

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -81,9 +81,22 @@
                                 <a class="dropdown-item" href="{{ url('/productos')}}">Productos</a> 
                                 </div>
 
-</li>
+                                </li>
 
-                        
+                                </li>
+
+                                <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Varios
+                                </a>
+
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ url('/')}}">Ordenes</a>
+                                <a class="dropdown-item" href="{{ url('/')}}">Pedidos</a>
+
+                                </div>
+                                </li>
+
 
                                 </ul>
 
